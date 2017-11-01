7186c18d15be944e8df9f8768cbf8a234a76a9fa
baolinet <bao.li.net@gmail.com>
2017-11-01 16:05:02 +0800

    init platform

---
+++ src/main/java/com/chanjetpay/GarlicBoltPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.chanjetpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarlicBoltPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarlicBoltPlatformApplication.class, args);
+	}
+}
