df987900b4501a40a8abdec56e28c0fc9b3fc7f1
baolinet <bao.li.net@gmail.com>
2017-11-13 20:04:03 +0800

    本地调用feign，配置框架

---
+++ src/main/java/com/chanjetpay/garlic/web/IndexController.java
@@ -0,0 +1,47 @@
+package com.chanjetpay.garlic.web;
+
+import com.chanjetpay.garlic.api.BlockService;
+import com.chanjetpay.garlic.dto.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+/**
+ * Created by libaoa on 2017/11/7.
+ */
+@Controller
+public class IndexController {
+
+	@Autowired
+	@Qualifier("blockService")
+	private BlockService blockService;
+
+	@RequestMapping("/")
+	public String index(){
+
+		List<UserDto> users = blockService.queryUsers("hehe哈哈哈");
+
+		for(UserDto user : users){
+			System.out.println(user);
+		}
+
+		return "index";
+	}
+
+
+	@RequestMapping("/save")
+	public String save(){
+
+		UserDto d1 = new UserDto();
+		d1.setNickName("张磊磊");
+		UserDto dto = blockService.saveUser(d1);
+
+		System.out.println(dto);
+
+
+		return "home";
+	}
+}
